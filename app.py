716f64e014b9a6f0b4b062a3b98603b6050d7b31
Saifali786 <41464847+Saifali786@users.noreply.github.com>
2023-02-23 18:31:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -72,5 +72,5 @@ def deleteS3File():
         return Response(status=200)  
         
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)        
+    app.run(host="0.0.0.0", debug=True)        
 
